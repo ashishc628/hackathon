444016c572a6cd6a4ddca5b6eba1f6da49dabd4d
Ashish chaudhary <36606232+ashishc628@users.noreply.github.com>
2025-11-30 07:37:14 +0530

    Update verification_intent.py

--- llm/verification_intent.py
+++ llm/verification_intent.py
@@ -3,7 +3,7 @@ import json
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from app.config import settings
+from config import settings
 
 llm_intent = ChatOpenAI(
     model=settings.OPENAI_MODEL,
